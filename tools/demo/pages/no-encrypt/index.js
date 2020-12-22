4e285595aec235470f31472b6258e7051a5837e2
IlhamTahir <yantaq@bilig.biz>
2020-12-22 21:02:15 +0800

    版本更新

--- tools/demo/pages/no-encrypt/index.js
+++ tools/demo/pages/no-encrypt/index.js
@@ -1,7 +1,8 @@
 Page({
   data: {
     imageData: [
-      'http://ese4a9b9c5d22x.pri.qiqiuyun.net/book-resource-6/20201021094357-mcj24tn8leswsosg/5d3f7e22090d07e7_img1?e=1607561685&token=ExRD5wolmUnwwITVeSEXDQXizfxTRp7vnaMKJbO-:gjLcUxS0n13Rejt7eZpd4t-NuKs='
+      'http://ese4a9b9c5d22x.pri.qiqiuyun.net/book-resource-6/20201021094357-mcj24tn8leswsosg/5d3f7e22090d07e7_img1?e=1607561685&token=ExRD5wolmUnwwITVeSEXDQXizfxTRp7vnaMKJbO-:gjLcUxS0n13Rejt7eZpd4t-NuKs=',
+      'https://ese4a9b9c5d22x.prissl.qiqiuyun.net/materiallib-2/20201214110104-h9mqtpl2jsowwk4s/a4ca618d945738af_img9?e=1608644078&token=ExRD5wolmUnwwITVeSEXDQXizfxTRp7vnaMKJbO-:39_3DwDYZwCDO0blfiN7Sd2Hj1g='
     ]
   }
 })
